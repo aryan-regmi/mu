738e2b917526a51e5da253c5c7f5bab0d7f4f570
Aryan Regmi <aryan_regmi@protonmail.com>
2024-07-31 19:50:15 -0600

    Consolidated tests into funcs

--- tests/unique_ptr_tests.cpp
+++ tests/unique_ptr_tests.cpp
@@ -17,88 +17,93 @@ struct Tst {
   auto clone() const -> Tst { return Tst{*this}; }
 };
 
-int main(void) {
+static auto singleObject() -> void {
   mem::CAllocator allocator{};
 
-  // Single object
-  {
-    auto val = UniquePtr<Tst>::create(&allocator);
-    assert(val->x == 1);
-    assert(val->y == 2);
-    assert(val->z == 3);
-    assert(val->b == true);
-    assert((*val).x == 1);
-    assert((*val).y == 2);
-    assert((*val).z == 3);
-    assert((*val).b == true);
-    const Tst* got = val.get();
-    assert(got->x == 1);
-    assert(got->y == 2);
-    assert(got->z == 3);
-    assert(got->b == true);
+  auto            val = UniquePtr<Tst>::create(&allocator);
+  assert(val->x == 1);
+  assert(val->y == 2);
+  assert(val->z == 3);
+  assert(val->b == true);
+  assert((*val).x == 1);
+  assert((*val).y == 2);
+  assert((*val).z == 3);
+  assert((*val).b == true);
+  const Tst* got = val.get();
+  assert(got->x == 1);
+  assert(got->y == 2);
+  assert(got->z == 3);
+  assert(got->b == true);
 
-    auto cloned = val.clone();
-    val->x      = 2;
-    assert(cloned->x == 1);
+  auto cloned = val.clone();
+  val->x      = 2;
+  assert(cloned->x == 1);
 
-    auto val2 = std::move(val);
-    assert(val.valid() == false);
-    assert(val2.valid() == true);
-    assert(val2->x == 2);
-    assert(val2->y == 2);
-    assert(val2->z == 3);
-    assert(val2->b == true);
+  auto val2 = std::move(val);
+  assert(val.valid() == false);
+  assert(val2.valid() == true);
+  assert(val2->x == 2);
+  assert(val2->y == 2);
+  assert(val2->z == 3);
+  assert(val2->b == true);
 
-    Tst* released = val2.release();
-    assert(val2.valid() == false);
-    assert(released->x == 2);
-    assert(released->y == 2);
-    assert(released->z == 3);
-    assert(released->b == true);
-    allocator.destroy(released);
+  Tst* released = val2.release();
+  assert(val2.valid() == false);
+  assert(released->x == 2);
+  assert(released->y == 2);
+  assert(released->z == 3);
+  assert(released->b == true);
+  allocator.destroy(released);
+}
+
+static auto sliceObjects() -> void {
+  mem::CAllocator allocator{};
+
+  auto            val = UniquePtr<Slice<Tst>>::create(&allocator, 2);
+  assert(val[0].x == 1);
+  assert(val[0].y == 2);
+  assert(val[0].z == 3);
+  assert(val[0].b == true);
+  assert(val[1].x == 1);
+  assert(val[1].y == 2);
+  assert(val[1].z == 3);
+  assert(val[1].b == true);
+  try {
+    assert(val[2].x == 1);
+    assert(val[2].y == 2);
+    assert(val[2].z == 3);
+    assert(val[2].b == true);
+  } catch (common::IndexOutOfBounds& e) {
+    io::Stdout().format("%s (index: %zu, lenght: %zu)", e.what(), e.idx, e.len);
   }
+
+  auto cloned = val.clone();
+  val[0].x    = 2;
+  assert(cloned[0].x == 1);
+
+  auto val2 = std::move(val);
+  assert(val.valid() == false);
+  assert(val2.valid() == true);
+  assert(val2[0].x == 2);
+  assert(val2[0].y == 2);
+  assert(val2[0].z == 3);
+  assert(val2[0].b == true);
+
+  Slice<Tst> released = val2.release();
+  assert(val2.valid() == false);
+  assert(released[0].x == 2);
+  assert(released[0].y == 2);
+  assert(released[0].z == 3);
+  assert(released[0].b == true);
+  allocator.free(released);
+}
+
+int main(void) {
+  // Single object
+  singleObject();
 
   // Slice of objects
-  {
-    auto val = UniquePtr<Slice<Tst>>::create(&allocator, 2);
-    assert(val[0].x == 1);
-    assert(val[0].y == 2);
-    assert(val[0].z == 3);
-    assert(val[0].b == true);
-    assert(val[1].x == 1);
-    assert(val[1].y == 2);
-    assert(val[1].z == 3);
-    assert(val[1].b == true);
-    try {
-      assert(val[2].x == 1);
-      assert(val[2].y == 2);
-      assert(val[2].z == 3);
-      assert(val[2].b == true);
-    } catch (common::IndexOutOfBounds& e) {
-      io::Stdout().format("%s (index: %zu, lenght: %zu)", e.what(), e.idx,
-                          e.len);
-    }
-
-    auto cloned = val.clone();
-    val[0].x    = 2;
-    assert(cloned[0].x == 1);
-
-    auto val2 = std::move(val);
-    assert(val.valid() == false);
-    assert(val2.valid() == true);
-    assert(val2[0].x == 2);
-    assert(val2[0].y == 2);
-    assert(val2[0].z == 3);
-    assert(val2[0].b == true);
-
-    Slice<Tst> released = val2.release();
-    assert(val2.valid() == false);
-    assert(released[0].x == 2);
-    assert(released[0].y == 2);
-    assert(released[0].z == 3);
-    assert(released[0].b == true);
-    allocator.free(released);
-  }
+  sliceObjects();
 
   return 0;
 }
