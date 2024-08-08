f73915462cb97af7cecbf6c211033e4d38835f35
Aryan Regmi <aryan_regmi@protonmail.com>
2024-08-08 16:42:45 -0600

    Added Enumerator

--- include/mu/iterable.h
+++ include/mu/iterable.h
@@ -1,14 +1,42 @@
 #ifndef MU_ITERABLE
 #define MU_ITERABLE
 
-#include "mu/optional.h"
-#include <concepts>
-#include <type_traits>
+#include "mu/optional.h"   // Optional
+#include "mu/primitives.h" // usize
+#include <concepts>        // same_as
+#include <tuple>
+
 namespace mu {
 
 template <class T>
 concept Iterable = requires(T self) {
   { self._nextImpl() } -> std::same_as<Optional<typename T::Item>>;
+};
+
+template <class Context, class Item> struct Iterator;
+
+template <class Context, class ItemType>
+class Enumerator : public Iterator<Enumerator<Context, ItemType>,
+                                   std::tuple<usize, Optional<ItemType>>> {
+public:
+  using Item = std::tuple<usize, Optional<ItemType>>;
+
+  explicit Enumerator(Context* iter)
+    requires(Iterable<Context>)
+      : it{iter} {}
+
+  auto _nextImpl() -> Optional<Item> {
+    Optional<ItemType> item = this->it->next();
+    if (item.isValid()) {
+      this->count += 1;
+      return Optional<Item>(std::make_tuple(this->count - 1, item));
+    }
+    return Optional<Item>();
+  }
+
+private:
+  Context* it;
+  usize    count = 0;
 };
 
 template <class Context, class Item> struct Iterator {
@@ -22,6 +50,7 @@ template <class Context, class Item> struct Iterator {
   template <typename F>
   auto forEach(F&& func) -> void
     requires requires(F&& func, Item& item) {
+      { Iterable<Context> };
       requires std::invocable<F, Item&>;
       { func(item) } -> std::same_as<void>;
     }
@@ -32,9 +61,11 @@ template <class Context, class Item> struct Iterator {
       func(item.unwrap());
       item = self->next();
     }
-    // for (Optional<Item>& item : self->next()) {
-    //   func(item);
-    // }
+  }
+
+  auto enumerate() -> Enumerator<Context, Item> {
+    Context* self = static_cast<Context*>(this);
+    return Enumerator<Context, Item>(self);
   }
 };
 
